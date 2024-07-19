a8284c540c4496e645037f6d99dfd11c400ee4cb
Victor Charpenay <victor.charpenay@emse.fr>
2024-07-19 12:26:51 +0200

    improve parsing of UA variable types

--- src/main/java/org/hypermedea/opcua/DataValueConverter.java
+++ src/main/java/org/hypermedea/opcua/DataValueConverter.java
@@ -1,26 +1,60 @@
 package org.hypermedea.opcua;
 
+import org.apache.jena.datatypes.RDFDatatype;
+import org.apache.jena.datatypes.xsd.XSDDatatype;
 import org.apache.jena.rdf.model.Literal;
 import org.apache.jena.rdf.model.ResourceFactory;
+import org.eclipse.milo.opcua.stack.core.BuiltinDataType;
 import org.eclipse.milo.opcua.stack.core.types.builtin.DataValue;
+import org.eclipse.milo.opcua.stack.core.types.builtin.ExpandedNodeId;
 import org.eclipse.milo.opcua.stack.core.types.builtin.Variant;
+
+import java.util.Optional;
 
 public class DataValueConverter {
 
     private DataValueConverter() {}
 
     public static Literal asLiteral(DataValue dv) {
-        return ResourceFactory.createTypedLiteral(dv.getValue().getValue());
+        Variant var = dv.getValue();
+        return ResourceFactory.createTypedLiteral(var.getValue().toString(), getDatatype(var));
     }
 
     public static DataValue asDataValue(Literal l) {
         Variant v = new Variant(l.asLiteral().getValue());
+        // TODO inverse function to getDatatype()
         return DataValue.valueOnly(v);
     }
 
     public static DataValue asDataValue(Literal l, String datatype) {
         return asDataValue(l);
         // TODO cast if possible
+    }
+
+    private static RDFDatatype getDatatype(Variant var) {
+        Optional<ExpandedNodeId> dtOpt = var.getDataType();
+        if (dtOpt.isEmpty()) return XSDDatatype.XSDstring;
+
+        ExpandedNodeId dt = dtOpt.get();
+
+        if (dt.equalTo(BuiltinDataType.Boolean.getNodeId())) return XSDDatatype.XSDboolean;
+
+        if (dt.equalTo(BuiltinDataType.SByte.getNodeId())) return XSDDatatype.XSDbyte;
+        if (dt.equalTo(BuiltinDataType.Int16.getNodeId())) return XSDDatatype.XSDshort;
+        if (dt.equalTo(BuiltinDataType.Int32.getNodeId())) return XSDDatatype.XSDint;
+        if (dt.equalTo(BuiltinDataType.Int64.getNodeId())) return XSDDatatype.XSDlong;
+
+        if (dt.equalTo(BuiltinDataType.Byte.getNodeId())) return XSDDatatype.XSDunsignedByte;
+        if (dt.equalTo(BuiltinDataType.UInt16.getNodeId())) return XSDDatatype.XSDunsignedShort;
+        if (dt.equalTo(BuiltinDataType.UInt32.getNodeId())) return XSDDatatype.XSDunsignedInt;
+        if (dt.equalTo(BuiltinDataType.UInt64.getNodeId())) return XSDDatatype.XSDunsignedLong;
+
+        if (dt.equalTo(BuiltinDataType.Float.getNodeId())) return XSDDatatype.XSDfloat;
+        if (dt.equalTo(BuiltinDataType.Double.getNodeId())) return XSDDatatype.XSDdouble;
+
+        if (dt.equalTo(BuiltinDataType.DateTime.getNodeId())) return XSDDatatype.XSDdateTime;
+
+        return XSDDatatype.XSDstring;
     }
 
 }
